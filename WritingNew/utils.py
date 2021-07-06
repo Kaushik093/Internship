40557c959b52f1f00388019a21a1ab3f3d9dd0fa
jackjack3006 <jvsanoj23@gmail.com>
2021-07-06 21:28:16 +0530

    Updating the new method

--- WritingNew/utils.py
+++ WritingNew/utils.py
@@ -3,4 +3,25 @@ import numpy as np
 
 def get(img):
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.GaussianBlur(grey, (5, 5), 1)
+    blur = cv2.GaussianBlur(grey, (5, 5), 1)
+    canny = cv2.Canny(blur, 100, 100)
+    kernel = np.ones((5, 5))
+    dilate = cv2.dilate(canny, kernel=kernel, iterations=3)
+    erode = cv2.erode(dilate, kernel, iterations=2)
+
+    minarea = 1000
+    finalContour = []
+
+    contours, heirarchy = cv2.findContours(erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if area > minarea:
+            peri = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+            bbox = cv2.boundingRect(approx)
+
+            cv2.drawContours(img, contour, -1, (0, 0, 255), 3)
+    
+    return img
---
+++ run.py
@@ -0,0 +1,24 @@
+from WritingNew import utils
+import cv2
+
+wCam, hCam = 610, 470
+
+capture = cv2.VideoCapture('clips/box1.mp4')
+capture.set(3, wCam)
+capture.set(4, hCam)
+
+while True:
+    ret, image = capture.read()
+
+    if not ret:
+        capture.release()
+        cv2.destroyAllWindows()
+        break
+
+    image = utils.get(image)
+
+    cv2.imshow('Detecting', image)
+
+    if cv2.waitKey(20) & 0xFF == ord('d'):
+        cv2.destroyAllWindows()
+        break
