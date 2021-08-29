bfbacf35e65e1081ce6fcfcec774be06d3264055
jackjack3006 <jvsanoj23@gmail.com>
2021-08-29 16:42:26 +0530

    Sample running code in the bottom

--- KIAM/src/SAMPLECODE.cpp
+++ KIAM/src/SAMPLECODE.cpp
@@ -395,3 +395,19 @@ void loop()
 
 
 }//end Loop.
+
+// #include <Arduino.h>
+// #define onboard 13
+
+// void setup()
+// {
+//     pinMode(onboard, OUTPUT);
+// }
+
+// void loop()
+// {
+//     digitalWrite(onboard, LOW);
+//     delay(1000);
+//     digitalWrite(onboard, HIGH);
+//     delay(1000);
+// }
