96b98bd63549f55ca251fca852167ac77232bf67
Kaushik093 <narayankaushik49@gmail.com>
2021-09-13 19:27:22 +0530

    hardcoding

--- Banana.py
+++ Banana.py
@@ -18,24 +18,26 @@ def empty(a):
 
 cv2.namedWindow("result")
 cv2.resizeWindow("result", framewidth, frameheight + 100)
-cv2.createTrackbar("scale", "result", 400, 1000, empty)
-cv2.createTrackbar("neig", "result", 8, 20, empty)
-cv2.createTrackbar("min area", "result", 0, 100000, empty)
-cv2.createTrackbar("brightness", "result", 180, 255, empty)
+# cv2.createTrackbar("scale", "result", 400, 1000, empty)
+# cv2.createTrackbar("neig", "result", 8, 20, empty)
+# cv2.createTrackbar("min area", "result", 0, 100000, empty)
+# cv2.createTrackbar("brightness", "result", 180, 255, empty)
 
 cascade = cv2.CascadeClassifier(path)
 
 while True:
-    camBrightness = cv2.getTrackbarPos("brightness", "result")
+    # camBrightness = cv2.getTrackbarPos("brightness", "result")
+    camBrightness=152
     cap.set(10, camBrightness)
     s, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    scaleval = 1 + (cv2.getTrackbarPos("scale", "result") / 1000)
-    neig = cv2.getTrackbarPos("neig", "result")
-    objects = cascade.detectMultiScale(gray, scaleval, neig)
+    # scaleval = 1 + (cv2.getTrackbarPos("scale", "result") / 1000)
+    # neig = cv2.getTrackbarPos("neig", "result")
+    objects = cascade.detectMultiScale(gray, 120, 5)
     for (x, y, w, h) in objects:
         area = w * h
-        minArea = cv2.getTrackbarPos("min area", "result")
+        # minArea = cv2.getTrackbarPos("min area", "result")
+        minArea=152
         if area > minArea:
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 3)
             cv2.putText(img, objectname, (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
